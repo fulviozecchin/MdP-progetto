de4c9646602be8cb3fc315337b6bf8e1804179d4
Fulvio Zecchin <fulviozecchin3@gmail.com>
2021-12-27 20:03:36 +0100

    corretto il main

--- src/main/java/com/unitelmasapienza/asciiart/imageeditor/MainEditor.java
+++ src/main/java/com/unitelmasapienza/asciiart/imageeditor/MainEditor.java
@@ -11,13 +11,10 @@ public class MainEditor {
 	
 	/**
 	 * Start up the application 
-	 * by setting setVisible to true on the ImageEditor instance
+	 * by getting the instance of application's controller.
 	 * @param args an array of command-line arguments for the application	 
 	 */
 	public static void main(String[] args)	{
-//		ImageEditor.getInstance().setVisible(true);
-		
-		//Test per provare le mie modifiche
 		ImageEditorController.getInstance();
 	}
 }
